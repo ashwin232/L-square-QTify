e71f42ff9ff01d8f185fe73192c5ec82fed366f3
ashwin232 <getashwinb4u@gmail.com>
2023-11-10 01:04:59 +0530

    Card-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Component/Navbar';
 import Hero from './Component/Hero/hero';
+import Card from './Component/Card/Card';
 
 function App() {
   return (
     <>
   <Navbar/>
   <Hero/>
+  <Card
+  followers='100M follows'
+  title='Bollywood songs'
+  />
   </>
   );
 }
